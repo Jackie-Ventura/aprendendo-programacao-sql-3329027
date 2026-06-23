078a304fd9f570b91d4d3f699c49baae719db246
Jackson Lourenço Ventura Rocha <jventuradesigner@gmail.com>
2026-06-23 02:08:32 +0000

    Exercicio 3

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,6 +1,6 @@
 -- Crie uma nova tabela no banco de dados
 CREATE TABLE salario_classe (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nivel TEXT,
-    salario INTEGER
+    id_salario INTEGER PRIMARY KEY AUTOINCREMENT,
+    salario INTEGER,
+    nivel VARCHAR(50)
 );
